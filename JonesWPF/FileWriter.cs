a89e28e0395386587621f81c2cf263da9808e762
Nikita Kretov <kretov995@gmail.com>
2016-03-01 00:10:03 +0300

    Configuration realisation.

--- JonesWPF/FileWriter.cs
+++ JonesWPF/FileWriter.cs
@@ -8,30 +8,95 @@ using System.Threading.Tasks;
 
 namespace JonesWPF
 {
+    //TODO подумать над именами
+    enum SelectedReportInf
+    {
+        X,
+        Y,
+        Time,
+        Temp,
+        Density,
+        Water,
+        Viscosity,
+        RelativeDeformation,
+        RockType,
+        Default
+    }
+
     static class FileWriter
     {
         static string outFileName;
+        static List<SelectedReportInf> Config;
 
         public static string SavingDirectory { get; set; }
         
+        public static void ConfigurateReport(List<SelectedReportInf> list)
+        {
+            Config = list;
+            Config.Sort();
+        }
+
         public static void SetOutFileName(string directory)
         {
             outFileName = directory.Split(new char[] { '\\' }).Last();
         }
+
         public static void Write(List<DataPoint[]> result)
         {
-            StreamWriter file = new StreamWriter($"{SavingDirectory}\\{outFileName}.txt");
+            var file = new StreamWriter($"{SavingDirectory}\\{outFileName}.txt");
 
             foreach (var column in result)
             {
+                //TODO писать заголовки столбцов в зависимости от конфига
                 file.WriteLine("Time\tTemp\tX\tY");
                 foreach (var dataPoint in column)
                 {
-                    file.WriteLine(string.Format("{0},{1},{2},{3}", dataPoint.Time, dataPoint.Temperature, dataPoint.X, dataPoint.Y));
+                    file.WriteLine(MakeString(dataPoint));
                 }
             }
             file.WriteLine("endOfFile");
             file.Close();
+        }
+
+        private static string MakeString(DataPoint dataPoint)
+        {
+            string result = $"{dataPoint.Id};";
+            foreach (var item in Config)
+            {
+                switch (item)
+                {
+                    case SelectedReportInf.X:
+                        result += $"{dataPoint.X};";
+                        break;
+                    case SelectedReportInf.Y:
+                        result += $"{dataPoint.Y};";
+                        break;
+                    case SelectedReportInf.Time:
+                        result += $"{dataPoint.Time};";
+                        break;
+                    case SelectedReportInf.Temp:
+                        result += $"{dataPoint.Temperature};";
+                        break;
+                    case SelectedReportInf.Density:
+                        //result += $"{dataPoint.Y};";
+                        break;
+                    case SelectedReportInf.Water:
+                        //result += $"{dataPoint.Y};";
+                        break;
+                    case SelectedReportInf.Viscosity:
+                        //result += $"{dataPoint.Y};";
+                        break;
+                    case SelectedReportInf.RelativeDeformation:
+                        //result += $"{dataPoint.Y};";
+                        break;
+                    case SelectedReportInf.RockType:
+                        //result += $"{dataPoint.Y};";
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return result;
         }
     }
 }
--- JonesWPF/ViewModels/ConfigurateOutputViewModel.cs
+++ JonesWPF/ViewModels/ConfigurateOutputViewModel.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using System.ComponentModel;
 
 namespace JonesWPF.ViewModels
 {
     class ConfigurateOutputViewModel : INotifyPropertyChanged
     {
+        //TODO подумать над именем этого массива!
+        List<SelectedReportInf> arr;
+
+        public ICommand CloseCommand { get; set; }
+
+        public ConfigurateOutputViewModel()
+        {
+            arr = new List<SelectedReportInf>();
+
+            CloseCommand = new RelayCommand(arg => CloseMethod());
+        }
+
         #region PropsForUI
 
-        private bool timeIsChecked = true;
+        private bool timeIsChecked;
         public bool TimeIsChecked
         {
             get { return timeIsChecked; }
@@ -19,23 +32,66 @@ namespace JonesWPF.ViewModels
                 if (timeIsChecked != value)
                 {
                     timeIsChecked = value;
-                    OnPropertyChanged(nameof(TimeIsChecked));
+                    OnPropertyChanged(nameof(TimeIsChecked), timeIsChecked ? SelectedReportInf.Time : SelectedReportInf.Default);
                 }
             }
         }
 
+        private bool tempIsChecked;
+        public bool TempIsChecked
+        {
+            get { return tempIsChecked; }
+            set {
+                if (tempIsChecked != value)
+                {
+                    tempIsChecked = value;
+                    OnPropertyChanged(nameof(TempIsChecked), tempIsChecked ? SelectedReportInf.Temp : SelectedReportInf.Default);
+                }
+            }
+        }
 
+        private bool xIsChecked;
+        public bool XIsChecked
+        {
+            get { return xIsChecked; }
+            set {
+                if (xIsChecked != value)
+                {
+                    xIsChecked = value;
+                    OnPropertyChanged(nameof(XIsChecked), xIsChecked ? SelectedReportInf.X : SelectedReportInf.Default);
+                }
+            }
+        }
+
+        private bool yIsChecked;
+        public bool YIsChecked
+        {
+            get { return yIsChecked; }
+            set
+            {
+                if (yIsChecked != value)
+                {
+                    yIsChecked = value;
+                    OnPropertyChanged(nameof(YIsChecked), yIsChecked ? SelectedReportInf.Y : SelectedReportInf.Default);
+                }
+            }
+        }
 
         #endregion
 
-        protected virtual void OnPropertyChanged(string propertyName)
+        private void CloseMethod()
+        {
+            FileWriter.ConfigurateReport(arr);
+        }
+
+        private void OnPropertyChanged(string propertyName, SelectedReportInf selectedReprotInf)
         {
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+                arr.Add(selectedReprotInf);
             }
         }
-
 
         public event PropertyChangedEventHandler PropertyChanged;
     }
